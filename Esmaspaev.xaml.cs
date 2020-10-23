90dd200997ddf7a29f24ff455daec8483027b4aa
Faskin <alex2002tyn@gmail.com>
2020-10-23 09:56:38 +0300

    Initial commit

---
+++ Esmaspaev.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Paevaplaan
+{
+    public partial class Esmaspaev : ContentPage
+    {
+        public Esmaspaev()
+        {
+            InitializeComponent();
+        }
+    }
+}
